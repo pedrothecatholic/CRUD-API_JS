5afc81bb05bb7b33800995ef438fffda65d873e7
Pedro Henrique de Sousa <pedrozica.phs21@gmail.com>
2021-12-10 10:42:48 -0300

    .

---
+++ CRUD_JS/README.md
@@ -0,0 +1,1 @@
+# crud-via-api-com-js
---
+++ CRUD_JS/js/main.js
@@ -0,0 +1,91 @@
+'use strict';
+
+import {
+    openModal,
+    closeModal
+} from './modal.js';
+
+import {
+    getProdutos,
+    postProduto,
+    deleteProduto
+} from './produtos.js';
+
+import {
+    imagePreview
+} from './imagePreview.js';
+
+const criarLinha = ({
+    id,
+    foto,
+    nome,
+    preco,
+    categoria
+}) => {
+    const linha = document.createElement('tr');
+    linha.innerHTML = `
+                    <td>
+                        <img src="${foto}" class="produto-image" />
+                    </td>
+                    <td>${nome}</td>
+                    <td>${preco}</td>
+                    <td>${categoria}</td>
+                    <td>
+                        <button type="button" class="button green" data-idproduto=${id}>
+                            editar
+                        </button>
+                        <button type="button" class="button red" data-idproduto=${id}>
+                            excluir
+                        </button>
+                    </td>`;
+
+    return linha;
+};
+
+const carregarTabela = async () => {
+    const container = document.querySelector('.records tbody');
+    const produtos = await getProdutos();
+    const linhas = produtos.map(criarLinha);
+
+    container.replaceChildren(...linhas);
+};
+
+const handleFile = () => imagePreview('inputFile', 'imagePreview');
+
+const salvarProduto = () => {
+    const produto = {
+        nome: document.getElementById('product').value,
+        preco: document.getElementById('price').value,
+        categoria: document.getElementById('category').value,
+        foto: document.getElementById('imagePreview').src,
+    };
+    postProduto(produto);
+    closeModal();
+};
+
+const handleClickTbody = ({
+    target
+}) => {
+    if (target.type === 'button') {
+        const acaoBotao = target.textContent.trim();
+        if (acaoBotao === 'excluir') {
+            deleteProduto(target.dataset.idproduto);
+            carregarTabela();
+        }
+    }
+};
+
+carregarTabela();
+
+// eventos
+document.getElementById('cadastrarCliente').addEventListener('click', openModal);
+
+document.getElementById('modalClose').addEventListener('click', closeModal);
+
+document.getElementById('cancel').addEventListener('click', closeModal);
+
+document.getElementById('inputFile').addEventListener('change', handleFile);
+
+document.getElementById('save').addEventListener('click', salvarProduto);
+
+document.querySelector('.records tbody').addEventListener('click', handleClickTbody);
